ffeb0e93b2ea0e95e8e28c4e2d5c94680890d159
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-03-25 23:35:02 +0600

    Clear button is working now

--- src/component/AllCart/AllCart.js
+++ src/component/AllCart/AllCart.js
@@ -3,20 +3,37 @@ import Cart from '../Carts/Cart';
 import './AllCart.css'
 const AllCart = () => {
     const [products, setProducts] = useState([]);
+    const [names, setnames] = useState([])
     useEffect(() => {
         fetch('Product.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+    const AddToCart = (props) => {
+        const { name } = props;
+        const getname = name
+        const newName = [...names, <br />, getname]
+        if (names.length > 6) {
+            alert('You can not add more then 5 watch')
+        } else {
+            setnames(newName)
+        }
+
+    }
+
     return (
         <div className='main-diiv'>
             <div className='all-cart'>
                 {
-                    products.map(data => <Cart products={data}></Cart>)
+                    products.map(data => <Cart key={data.id} AddToCart={AddToCart} products={data}></Cart>)
                 }
             </div>
             <div className='display-name'>
-                <h1>display name</h1>
+                <h1>display name </h1>
+                <h2 className='added-cart-name'>{names}</h2>
+                <button className='display-rendom'>Reandom</button>
+                <br />
+                <button onClick={() => setnames([])} className='display-delets'>Clear All</button>
             </div>
         </div>
     );
--- src/component/Carts/Cart.js
+++ src/component/Carts/Cart.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './Carts.css'
 const Cart = (props) => {
     const { name, balance, img } = props.products
-    console.log(props)
     return (
         <div className='single-cart'>
             <img className='cart-img' src={img} alt="" />
             <div className='cart-info'>
                 <h1>{name}</h1>
                 <h2>price :{balance}</h2>
-                <button className='cart-btn'>Add To Card</button>
+                <button onClick={() => props.AddToCart(props.products)} className='cart-btn'>Add To Card</button>
             </div>
 
         </div>
